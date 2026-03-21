c3d58cfc96ebe3ffc09ff7e3e85366e7e41ebd9f
franciscorojasp <francisco.rojasp@gmail.com>
2026-03-21 18:46:56 -0400

    Fix build: remove unused AnimatePresence in Odontograma

--- src/pages/Odontograma.tsx
+++ src/pages/Odontograma.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { getPacientes, getOdontograma, saveOdontograma, type Paciente } from '../api';
 import { useClinica } from '../contexts/ClinicaContext';
 
